38e6057e5f3dad90428782a39cbac98ba9e8e7b8
Antonio Alberto Gutierrez-Jaramillo <antoniog1@login3.cm.cluster>
2019-02-18 11:17:08 -0700

    still a work in progress

---
+++ assignments/05-python-proteins/translate_proteins.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Author : antoniog1
+Date   : 2019-02-14
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+import os
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Argparse Python script',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('string', metavar='str', 
+                        help='DNA/RNA sequence')
+    parser.add_argument('-c', '--codons', help='A file with codon translations',
+                        metavar='FILE', type=str, default=None)
+    parser.add_argument('-o', '--output', help='Output filename',
+                        metavar='FILE', type=str, default='out.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    codons = args.codons
+    input_string = args.string
+    outfile = args.output
+
+    k = 3 
+    n = len(input_string) - k + 1
+
+
+    if codons is None:
+        die('codon file required')
+    if not os.path.isfile(codons):
+        die('--codons "{}" is not a file'.format(codons))
+    codonTable = {}
+    with open(codons) as cFile:
+        for line in cFile:
+            inCodon, protein = line.split()
+            codonTable[inCodon] = protein
+    with open(outfile, 'wt') as out_fh:
+        for i in range(0, n, k): 
+            inCodon = input_string[i:i+k]
+            out_fh.write(codonTable.get(inCodon,'-')) 
+    out_fh.close()
+    print('Output written to "{}"'.format(outfile)) 
+    
+    
+       
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
