36299d8ec0b57be9c351ab7500e78927b0ca29b5
Antonio Gutierrez-Jaramillo <antoniogutierrez-jaramillo@dhcp-10-132-134-214.uawifi.arizona.edu>
2019-03-22 16:27:39 -0700

    all done

--- assignments/10-war/war.py
+++ assignments/10-war/war.py
@@ -48,10 +48,10 @@ def die(msg='Something bad happened'):
 def main():
     """Make a jazz noise here"""
     args = get_args()
-    seed = args.seed
+    seed_number = args.seed
     
-    if seed is not None:
-        random.seed(seed) 
+    if seed_number is not None:
+        random.seed(seed_number) 
     
     unicode_symbols = list('♥♠♣♦')
     a = list('234567890') + list('JQKA')
@@ -63,27 +63,89 @@ def main():
     n=0
     player_two_score = 0
     player_one_score = 0
+    one_value = 0
+    two_value = 0 
+    
     while n < 52:
-        Player_one_card = deck[n]
-        Player_two_card = deck[n+1]
+        Player_one_card = list.pop(deck) 
+        Player_two_card = list.pop(deck) 
         
-        if Player_one_card[1] > Player_two_card[1]:
+        if Player_one_card[1] == '2':
+            one_value = 2
+        elif Player_one_card[1] == '3':
+            one_value = 3
+        elif Player_one_card[1] == '4':
+            one_value = 4
+        elif Player_one_card[1] == '5':
+            one_value = 5
+        elif Player_one_card[1] == '6':
+            one_value = 6
+        elif Player_one_card[1] == '7':
+            one_value = 7
+        elif Player_one_card[1] == '8':
+            one_value = 8
+        elif Player_one_card[1] == '9':
+            one_value = 9
+        elif Player_one_card[1] == '0':
+            one_value = 10
+        elif Player_one_card[1] == 'J':
+            one_value = 11
+        elif Player_one_card[1] == 'Q':
+            one_value = 12
+        elif Player_one_card[1] == 'K':
+            one_value = 13
+        else:
+            one_value = 14
+            
+        if Player_two_card[1] == '2':
+            two_value = 2
+        elif Player_two_card[1] == '3':
+            two_value = 3
+        elif Player_two_card[1] == '4':
+            two_value = 4
+        elif Player_two_card[1] == '5':
+            two_value = 5
+        elif Player_two_card[1] == '6':
+            two_value = 6
+        elif Player_two_card[1] == '7':
+            two_value = 7
+        elif Player_two_card[1] == '8':
+            two_value = 8
+        elif Player_two_card[1] == '9':
+            two_value = 9
+        elif Player_two_card[1] == '0':
+            two_value = 10
+        elif Player_two_card[1] == 'J':
+            two_value = 11
+        elif Player_two_card[1] == 'Q':
+            two_value = 12
+        elif Player_two_card[1] == 'K':
+            two_value = 13
+        else:
+            two_value = 14     
+            
+        if one_value > two_value:     
+        #if Player_one_card[1] > Player_two_card[1]:
             hand_winner = 'P1'
             player_one_score += 1
-        elif Player_two_card > Player_one_card:
+
+        #elif Player_two_card[1] > Player_one_card[1]:
+        elif one_value < two_value:
             hand_winner = 'P2'
             player_two_score +=1
         else:
             hand_winner = 'WAR!'
+            
+            
         if Player_one_card[1] == '0':
-            print(' {}1{}'.format(Player_one_card[0],Player_one_card[1]),end = '')
+            print('{}1{}'.format(Player_one_card[0],Player_one_card[1]),end = ' ')
         else: 
-            print('  {}{}'.format(Player_one_card[0],Player_one_card[1]),end = '')
+            print(' {}{}'.format(Player_one_card[0],Player_one_card[1]),end = ' ')
             
         if Player_two_card[1] == '0':
-            print(' {}1{}'.format(Player_two_card[0],Player_two_card[1]),end = ' ')
+            print('{}1{}'.format(Player_two_card[0],Player_two_card[1]),end = ' ')
         else:
-            print('  {}{}'.format(Player_two_card[0],Player_two_card[1]),end = ' ')
+            print(' {}{}'.format(Player_two_card[0],Player_two_card[1]),end = ' ')
         print('{}'.format(hand_winner))
         n += 2
     if player_two_score > player_one_score:
