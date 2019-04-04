e85f5715b2f158abc2c1be4e68a07fae60b3ae59
Antonio Gutierrez-Jaramillo <antoniogutierrez-jaramillo@dhcp-10-132-161-36.uawifi.arizona.edu>
2019-04-04 09:57:57 -0700

    still working

--- assignments/12-regex-passwords/pass.py
+++ assignments/12-regex-passwords/pass.py
@@ -1,32 +1,66 @@
 #!/usr/bin/env python3
 """
-Author : kyclark
-Date   : 2019-04-01
+Author : antoniogutierrez-jaramillo
+Date   : 2019-04-04
 Purpose: Rock the Casbah
 """
 
-import os
-import re
+import argparse
 import sys
+import re 
+
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Argparse Python script',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'password', metavar='PASSWORD', help='baseline password')
+
+    parser.add_argument(
+       'alternate',
+        metavar='ALT',
+        help='alternative password')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
 
 
 # --------------------------------------------------
 def main():
-    args = sys.argv[1:]
+    """Make a jazz noise here"""
+    args = get_args()
+    base_pass = args.password
+    alt_pass = args.alternate 
 
-    if len(args) != 2:
-        print('Usage: {} PASSWORD ALT'.format(os.path.basename(sys.argv[0])))
-        sys.exit(1)
+    pass_re = re.compile(.{0,1}(\w).{0,1}) 
+ 
+    password = base_pass.upper() 
+    alternative = alt_pass.upper()
+    
+    #if len(alternative) 
+    if alternative == password:
+        print("ok") 
+    else:
+        print("nah") 
 
-    password, alt = args
-
-    ucfirst = password[0].upper() + password[1:]
-
-    ok = (password == alt) or (password.upper() == alt) or (
-        ucfirst == alt) or re.match('.?' + password + '.?', alt)
-
-    print('ok' if ok else 'nah')
-
+   
 
 # --------------------------------------------------
-main()
+if __name__ == '__main__':
+    main()
