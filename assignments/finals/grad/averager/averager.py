79c6c7c94ba3c6ef99815a991eb528611cb902c5
Antonio Gutierrez-Jaramillo <antoniogutierrez-jaramillo@dhcp-10-134-229-141.uawifi.arizona.edu>
2019-05-07 09:13:28 -0700

    all done!

---
+++ assignments/finals/grad/averager/averager.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Author : antoniogutierrez-jaramillo
+Date   : 2019-05-07
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+import sys
+import re
+import logging
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Argparse Python script',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'FILE', metavar='FILE', help='Input file(s)',nargs='+')
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    files = args.FILE
+    #l = [] 
+    for file_entry in files:
+        l=[]
+        n=0
+        
+        if not os.path.isfile(file_entry):
+            print('"{}" is not a file'.format(file_entry),file=sys.stderr)
+            continue
+        with open(file_entry,"r") as fh:
+        #print('ok')
+            for line in fh:
+                #print(line)
+                for i in re.findall(r"[-+]?\d*\.\d+|[+-]?\d+",line):
+                    l.append(i) 
+                    #print(i)
+            n = len(l)
+            if n is 0:
+                file_average = 0.00
+                print('{:10.02f}: {}'.format(file_average,os.path.basename(file_entry)))
+            else:
+                l = [float(j) for j in l]
+                file_average = sum(l)/n
+                print('{:10.02f}: {}'.format(file_average,os.path.basename(file_entry)))
+                
+            
+            
+        
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
